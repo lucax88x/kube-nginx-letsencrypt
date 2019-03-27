ccf6e337ba0240ace7dc981a1b9ead337f85a2cb
Vandoolaeghe, Clément <clement.vandoolaeghe@ercom.fr>
2019-03-27 11:53:52 +0100

    Mod secret opaque to tls

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ -z $EMAIL || -z $DOMAINS || -z $SECRET || -z $DEPLOYMENT ]]; then
-	echo "EMAIL, DOMAINS, SECERT, and DEPLOYMENT env vars required"
+if [[ -z $EMAIL || -z $DOMAINS || -z $SECRET ]]; then
+	echo "EMAIL, DOMAINS, SECERT env vars required"
 	env
 	exit 1
 fi
@@ -28,15 +28,15 @@ cat /secret-patch-template.json | \
 ls /secret-patch.json || exit 1
 
 # update secret
-curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
+curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/secret-patch.json https://kubernetes.default/api/v1/namespaces/${NAMESPACE}/secrets/${SECRET}
 
-cat /deployment-patch-template.json | \
-	sed "s/TLSUPDATED/$(date)/" | \
-	sed "s/NAMESPACE/${NAMESPACE}/" | \
-	sed "s/NAME/${DEPLOYMENT}/" \
-	> /deployment-patch.json
+# cat /deployment-patch-template.json | \
+# 	sed "s/TLSUPDATED/$(date)/" | \
+# 	sed "s/NAMESPACE/${NAMESPACE}/" | \
+# 	sed "s/NAME/${DEPLOYMENT}/" \
+# 	> /deployment-patch.json
 
-ls /deployment-patch.json || exit 1
+# ls /deployment-patch.json || exit 1
 
-# update pod spec on ingress deployment to trigger redeploy
-curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/deployment-patch.json https://kubernetes/apis/extensions/v1beta1/namespaces/${NAMESPACE}/deployments/${DEPLOYMENT}
+# # update pod spec on ingress deployment to trigger redeploy
+# curl -v --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt -H "Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)" -k -v -XPATCH  -H "Accept: application/json, */*" -H "Content-Type: application/strategic-merge-patch+json" -d @/deployment-patch.json https://kubernetes.default/apis/extensions/v1beta1/namespaces/${NAMESPACE}/deployments/${DEPLOYMENT}
